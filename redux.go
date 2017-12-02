c8763343abb640bcedd82c5954cda800c1344c3a
routedan <dannypsnl@gmail.com>
2017-12-02 15:45:58 +0800

    when only one reducer, use GetStateD() method get defaultState

--- redux.go
+++ redux.go
@@ -26,26 +26,46 @@ type store struct {
 	// for example: "counter" mapping to counter reducer.
 	// when use GetState["counter"], we will got the current state of "counter" key's mapping target.
 	GetState map[string]interface{}
+	// defaultState only work when we don't have several reducer
+	defaultState interface{}
 }
 
-func NewStore() *store {
-	return &store{
+func NewStore(defaultReducer Reducer, reducers ...Reducer) *store {
+	s := &store{
 		GetState: make(map[string]interface{}),
 	}
+	if len(reducers) == 0 {
+		return &store{
+			defaultState: defaultReducer(nil, Action{}),
+			reducers:     []Reducer{defaultReducer},
+		}
+	}
+	s.NewReducer(defaultReducer)
+	for _, reducer := range reducers {
+		s.NewReducer(reducer)
+	}
+	return s
 }
 
 func (s *store) NewReducer(reducer Reducer) {
 	// this code will get package.function_name, so we drop package part
 	func_name := runtime.FuncForPC(reflect.ValueOf(reducer).Pointer()).Name()
 	func_name = func_name[strings.LastIndexByte(func_name, '.')+1:]
-	s.GetState[func_name] = reducer(s.GetState[func_name], Action{})
+	s.GetState[func_name] = reducer(nil, Action{})
 	s.reducers = append(s.reducers, reducer)
 }
 
 func (s *store) Dispatch(act *Action) {
+	if s.defaultState != nil {
+		s.defaultState = s.reducers[0](s.defaultState, *act)
+		return
+	}
 	for _, reducer := range s.reducers {
 		func_name := runtime.FuncForPC(reflect.ValueOf(reducer).Pointer()).Name()
 		func_name = func_name[strings.LastIndexByte(func_name, '.')+1:]
 		s.GetState[func_name] = reducer(s.GetState[func_name], *act)
 	}
+}
+func (s *store) GetStateD() interface{} {
+	return s.defaultState
 }
--- redux_test.go
+++ redux_test.go
@@ -4,12 +4,12 @@ import (
 	"testing"
 )
 
-func counter(state interface{}, act Action) interface{} {
+func counter(state interface{}, action Action) interface{} {
 	// The initial state
 	if state == nil {
 		return 0
 	}
-	switch act.Type {
+	switch action.Type {
 	case "INC":
 		return state.(int) + 1
 	case "DEC":
@@ -19,11 +19,32 @@ func counter(state interface{}, act Action) interface{} {
 	}
 }
 
+func jump(state interface{}, action Action) interface{} {
+	if state == nil {
+		return "TOP"
+	}
+	switch action.Type {
+	case "JUMP":
+		return "TOP"
+	case "FALL":
+		return "DOWN"
+	default:
+		return state
+	}
+}
+
 func TestStore(t *testing.T) {
-	store := NewStore()
-	store.NewReducer(counter)
+	store := NewStore(counter)
 	store.Dispatch(SendAction("INC"))
-	if store.GetState["counter"] != 1 {
-		t.Errorf("State should be 1 now, but it's: %v", store.GetState["counter"])
+	if store.GetStateD() != 1 {
+		t.Errorf("State should be 1 now, but it's: %v", store.GetStateD())
+	}
+	store = NewStore(counter, jump)
+	store.Dispatch(SendAction("JUMP"))
+	if store.GetState["jump"] != "TOP" {
+		t.Error(`Error`)
+	}
+	if store.GetState["counter"] != 0 {
+		t.Error(`initial state of counter is wrong`)
 	}
 }
