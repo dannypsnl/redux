da3f3ec992f38ccac479b68f1831a597ff428fa2
dannypsnl <dannypsnl@gmail.com>
2017-12-07 08:56:17 +0800

    ignore test(try some language sepc), and add TODO

--- serialize.go
+++ serialize.go
@@ -8,6 +8,7 @@ import (
 func (s *Store) JSON() (str string) {
 	str += "{\n"
 	for k, v := range s.state {
+		// TODO: fmt state is not useful, maybe use reflect get the type of state, then use json.Unmarshal jsonfy state.
 		str += "  \"" + k + "\":" + fmt.Sprintf("%#v,\n", v)
 	}
 	str = str[:len(str)-2] + "\n"
