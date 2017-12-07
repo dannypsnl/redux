7042075a8c43e1df03767c077eb77901c3e33441
dannypsnl <dannypsnl@gmail.com>
2017-12-07 09:13:51 +0800

    use reflect with json.Marshal, Thanks God, we can use interface but reflect the actual type.

--- serialize.go
+++ serialize.go
@@ -1,15 +1,31 @@
 package redux
 
 import (
+	_ "encoding/json"
 	"fmt"
+	"reflect"
 )
 
 // JSON serialize state of store to JSON format string
 func (s *Store) JSON() (str string) {
+	var b interface{}
 	str += "{\n"
 	for k, v := range s.state {
 		// TODO: fmt state is not useful, maybe use reflect get the type of state, then use json.Unmarshal jsonfy state.
-		str += "  \"" + k + "\":" + fmt.Sprintf("%#v,\n", v)
+		rv := reflect.ValueOf(v)
+		switch rv.Kind() {
+		case reflect.Struct:
+		case reflect.Interface:
+			fmt.Println("everything is interface{}")
+		case reflect.Int:
+			//fmt.Println("int")
+			b = v
+		case reflect.String:
+			b = v
+		}
+		//b, _ := json.Marshal(v)
+		str += "  \"" + k + "\":" + fmt.Sprintf("%#v,\n", b)
+		// fmt.Sprintf("%#v,\n", v)
 	}
 	str = str[:len(str)-2] + "\n"
 	str += "}"
--- serialize_test.go
+++ serialize_test.go
@@ -2,11 +2,41 @@ package redux
 
 import "testing"
 
+var users = map[string]string{
+	"danny": "1234",
+}
+
+func login(state interface{}, action Action) interface{} {
+	if state == nil {
+		return "Guest"
+	}
+	switch action.Type {
+	case "login":
+		if users[action.Args["user"].(string)] == action.Args["password"] {
+			return action.Args["user"].(string) + " Login"
+		}
+		return state
+	default:
+		return state
+	}
+}
+
 func TestSerialize(t *testing.T) {
-	store := NewStore(counter)
+	store := NewStore(counter, login)
+	expected := "{\n  \"counter\":0,\n  \"login\":\"Guest\"\n}"
+	if store.JSON() != expected {
+		t.Errorf("serialized result is not expected, expected: %s, actual: %s", expected, store.JSON())
+	}
 	store.Dispatch(SendAction("INC"))
 	store.Dispatch(SendAction("INC"))
-	expected := "{\n  \"counter\":2\n}"
+	store.Dispatch(&Action{
+		Type: "login",
+		Args: map[string]interface{}{
+			"user":     "danny",
+			"password": "1234",
+		},
+	})
+	expected = "{\n  \"counter\":2,\n  \"login\":\"danny Login\"\n}"
 	if store.JSON() != expected {
 		t.Errorf("serialized result is not expected, expected: %s, actual: %s", expected, store.JSON())
 	}
