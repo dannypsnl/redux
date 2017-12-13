7c996b85cb37e3b0caf3fb105097ebf137a51677
dannypsnl <dannypsnl@gmail.com>
2017-12-13 08:15:41 +0800

    reflect serialize code, json Marshal can do all the things we need at here

--- serialize.go
+++ serialize.go
@@ -3,7 +3,6 @@ package redux
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
 )
 
 // JSON serialize state of store to JSON format string
@@ -11,17 +10,8 @@ func (s *Store) JSON() (str string) {
 	var formatString string
 	str += "{\n"
 	for k, v := range s.state {
-		// TODO: fmt state is not useful, maybe use reflect get the type of state, then use json.Unmarshal jsonfy state.
-		rv := reflect.ValueOf(v)
-		switch rv.Kind() {
-		case reflect.Struct:
-			b, _ := json.Marshal(v)
-			formatString = fmt.Sprintf("%s", b)
-		case reflect.Int:
-			formatString = fmt.Sprintf("%#v", v)
-		case reflect.String:
-			formatString = fmt.Sprintf("%#v", v)
-		}
+		b, _ := json.Marshal(v)
+		formatString = fmt.Sprintf("%s", b)
 		str += "  \"" + k + "\":" + formatString + ",\n"
 	}
 	str = str[:len(str)-2] + "\n"
