b92a55782d7be9d54b0b466d6d6cdc85716958b8
dannypsnl <dannypsnl@gmail.com>
2017-12-17 16:06:02 +0800

    create a gui example

--- example/gui/main.go
+++ example/gui/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/andlabs/ui"
+	"github.com/dannypsnl/redux"
+)
+
+func counter(state interface{}, act redux.Action) interface{} {
+	if state == nil {
+		return 0
+	}
+	switch act.Type {
+	case "INC":
+		return state.(int) + 1
+	case "DEC":
+		return state.(int) - 1
+	default:
+		return state
+	}
+}
 
 func main() {
-	fmt.Println("vim-go")
+	store := redux.NewStore(counter)
+	err := ui.Main(func() {
+		buttonInc := ui.NewButton("+")
+		buttonDec := ui.NewButton("-")
+		number := ui.NewLabel("")
+		box := ui.NewVerticalBox()
+		box.Append(buttonInc, false)
+		box.Append(buttonDec, false)
+		box.Append(number, false)
+		window := ui.NewWindow("Hello", 200, 100, false)
+		window.SetChild(box)
+		buttonInc.OnClicked(func(*ui.Button) {
+			store.Dispatch(redux.SendAction("INC"))
+			s := fmt.Sprintf("Number is:%d", store.GetState("counter").(int))
+			number.SetText(s)
+		})
+		buttonDec.OnClicked(func(*ui.Button) {
+			store.Dispatch(redux.SendAction("DEC"))
+			s := fmt.Sprintf("Number is:%d", store.GetState("counter").(int))
+			number.SetText(s)
+		})
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		panic(err)
+	}
 }
