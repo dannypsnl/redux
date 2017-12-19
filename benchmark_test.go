d46a89e86ae1b9454afe832a95777a22679f80b4
dannypsnl <dannypsnl@gmail.com>
2017-12-19 14:27:20 +0800

    test benchmark of dispatchC

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,12 +1,11 @@
 package redux
 
 import (
-	"sync"
 	"testing"
 	"time"
 )
 
-func BenchmarkEasySubscribe(b *testing.B) {
+func BenchmarkANormalCaseSubscribe(b *testing.B) {
 	store := NewStore(counter, jump)
 	store.Subscribe(func() {
 		expectedState := "TOP"
@@ -14,62 +13,79 @@ func BenchmarkEasySubscribe(b *testing.B) {
 			b.Errorf("Expected: %v, Actual: %v", expectedState, store.GetState("jump"))
 		}
 	})
-	store.Subscribe(func() {
-		expectedState := "TOP"
-		if store.GetState("jump") != expectedState {
-			b.Errorf("Expected: %v, Actual: %v", expectedState, store.GetState("jump"))
-		}
-	})
 	for i := 0; i < b.N; i++ {
 		store.Dispatch(SendAction("JUMP"))
 	}
 }
 
-func BenchmarkHeavySubscribe(b *testing.B) {
+func BenchmarkSleep1msSubscribe(b *testing.B) {
 	store := NewStore(counter, jump)
-	store.Subscribe(func() {
-		time.Sleep(1 * time.Millisecond)
-	})
-	store.Subscribe(func() {
-		time.Sleep(1 * time.Millisecond)
-	})
+	for i := 0; i < 10; i++ {
+		store.Subscribe(func() {
+			time.Sleep(1 * time.Millisecond)
+		})
+	}
 	for i := 0; i < b.N; i++ {
 		store.Dispatch(SendAction("JUMP"))
 	}
 }
 
-func BenchmarkAlotEasySubscribe(b *testing.B) {
-	n := 0
+func BenchmarkSleep1msSubscribeByDispatchC(b *testing.B) {
 	store := NewStore(counter, jump)
-	for i := 0; i < 100000000; i++ {
-		n++
+	for i := 0; i < 10; i++ {
+		store.Subscribe(func() {
+			time.Sleep(1 * time.Millisecond)
+		})
+	}
+	for i := 0; i < b.N; i++ {
+		store.dispatchC(SendAction("JUMP"))
+	}
+}
+
+func BenchmarkALotSleep250nsSubscribe(b *testing.B) {
+	store := NewStore(counter, jump)
+	for i := 0; i < 10000; i++ {
+		store.Subscribe(func() {
+			time.Sleep(250 * time.Nanosecond)
+		})
 	}
 	for i := 0; i < b.N; i++ {
 		store.Dispatch(SendAction("JUMP"))
 	}
 }
 
-func Benchmark10000workConcurrent(b *testing.B) {
+func BenchmarkALotSleep250nsSubscribeByDispatchC(b *testing.B) {
+	store := NewStore(counter, jump)
+	for i := 0; i < 10000; i++ {
+		store.Subscribe(func() {
+			time.Sleep(250 * time.Nanosecond)
+		})
+	}
 	for i := 0; i < b.N; i++ {
-		for i := 0; i < 10000; i++ {
-			var wg sync.WaitGroup
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				time.Sleep(1 * time.Nanosecond)
-			}()
-			wg.Wait()
-		}
+		store.dispatchC(SendAction("JUMP"))
 	}
 }
 
-func Benchmark10000work(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		// If quest cost Nanosecond, sequence is faster than concurrency way.
-		// I think the problem is paging
-		// It about 3~4 time
-		for i := 0; i < 10000; i++ {
+func BenchmarkALotSleep1nsSubscribe(b *testing.B) {
+	store := NewStore(counter, jump)
+	for i := 0; i < 10000; i++ {
+		store.Subscribe(func() {
 			time.Sleep(1 * time.Nanosecond)
-		}
+		})
+	}
+	for i := 0; i < b.N; i++ {
+		store.Dispatch(SendAction("JUMP"))
+	}
+}
+
+func BenchmarkALotSleep1nsSubscribeByDispatchC(b *testing.B) {
+	store := NewStore(counter, jump)
+	for i := 0; i < 10000; i++ {
+		store.Subscribe(func() {
+			time.Sleep(1 * time.Nanosecond)
+		})
+	}
+	for i := 0; i < b.N; i++ {
+		store.dispatchC(SendAction("JUMP"))
 	}
 }
