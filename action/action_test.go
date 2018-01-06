bb13484577db1021f7302b85ce627f159475fbd5
dannypsnl <dannypsnl@gmail.com>
2018-01-06 20:48:12 +0800

    rename tests & improve comment of test

--- action/action_test.go
+++ action/action_test.go
@@ -4,17 +4,20 @@ import (
 	"testing"
 )
 
-func TestActionArgsInit(t *testing.T) {
+func TestArgsHadBeenInit(t *testing.T) {
 	defer func() {
+		// If panic, we can recover it
+		// So we can know Args didn't be initialize
 		if r := recover(); r != nil {
 			t.Error(`Args did not be init`)
 		}
 	}()
 	act := New("TEST_MESSAGE")
+	// If didn't, this operation will cause panic
 	act.Args["author"] = "danny"
 }
 
-func TestActionFluentAPI(t *testing.T) {
+func TestArgCanWork(t *testing.T) {
 	act := New("TEST_MESSAGE").
 		Arg("author", "danny").
 		Arg("age", 20)
