eb286bf163a37705c49ac1e2637797b8d1015400
dannypsnl <dannypsnl@gmail.com>
2018-01-06 20:39:59 +0800

    move def for test into reducer_for_test

--- store/reducers_for_test.go
+++ store/reducers_for_test.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"github.com/dannypsnl/redux/action"
+	"strings"
 )
 
 func counter(state interface{}, action action.Action) interface{} {
@@ -32,9 +33,75 @@ func jump(state interface{}, action action.Action) interface{} {
 	}
 }
 
+var users = map[string]string{
+	"danny": "1234",
+}
+
+func login(state interface{}, action action.Action) interface{} {
+	if state == nil {
+		return "Guest"
+	}
+	switch action.Type {
+	case "login":
+		if users[action.Args["user"].(string)] == action.Args["password"] {
+			return action.Args["user"].(string) + " Login"
+		}
+		return state
+	default:
+		return state
+	}
+}
+
 func foo(state interface{}, act action.Action) interface{} {
 	if state == nil {
 		return func() {}
+	}
+	return state
+}
+
+type file struct {
+	// Ext mean extension
+	Ext string `json:"ext"`
+	// two mod: +x, +d
+	Mod string `json:"mod"`
+}
+
+func fileUpdator(state interface{}, act action.Action) interface{} {
+	if state == nil {
+		return &file{
+			Ext: "elz",
+			Mod: "+x",
+		}
+	}
+	switch act.Type {
+	case "chmod":
+		if act.Args["mod"] == "+x" && !strings.Contains(state.(file).Mod, "+x") {
+			return &file{
+				Ext: state.(file).Ext,
+				Mod: state.(file).Mod + "+x",
+			}
+		} else if act.Args["mod"] == "+d" && !strings.Contains(state.(file).Mod, "+d") {
+			return &file{
+				Ext: state.(file).Ext,
+				Mod: state.(file).Mod + "+d",
+			}
+		} else if act.Args["mod"] == "-d" {
+			strings.Replace(state.(file).Mod, "+d", "", -1)
+		} else if act.Args["mod"] == "-x" {
+			strings.Replace(state.(file).Mod, "+x", "", -1)
+		} else {
+			return state
+		}
+	case "new ext":
+		if act.Args["Ext"] != nil {
+			return &file{
+				Ext: act.Args["Ext"].(string),
+				Mod: state.(file).Mod,
+			}
+		}
+		return state
+	default:
+		return state
 	}
 	return state
 }
--- store/serialize_test.go
+++ store/serialize_test.go
@@ -3,28 +3,8 @@ package store
 import (
 	"encoding/json"
 	"github.com/dannypsnl/redux/action"
-	"strings"
 	"testing"
 )
-
-var users = map[string]string{
-	"danny": "1234",
-}
-
-func login(state interface{}, action action.Action) interface{} {
-	if state == nil {
-		return "Guest"
-	}
-	switch action.Type {
-	case "login":
-		if users[action.Args["user"].(string)] == action.Args["password"] {
-			return action.Args["user"].(string) + " Login"
-		}
-		return state
-	default:
-		return state
-	}
-}
 
 func TestSerialize(t *testing.T) {
 	type StoreByCounterLogin struct {
@@ -49,53 +29,6 @@ func TestSerialize(t *testing.T) {
 		action.New("login").
 			Arg("user", "danny").
 			Arg("password", 1234))
-}
-
-type file struct {
-	// Ext mean extension
-	Ext string `json:"ext"`
-	// two mod: +x, +d
-	Mod string `json:"mod"`
-}
-
-func fileUpdator(state interface{}, act action.Action) interface{} {
-	if state == nil {
-		return &file{
-			Ext: "elz",
-			Mod: "+x",
-		}
-	}
-	switch act.Type {
-	case "chmod":
-		if act.Args["mod"] == "+x" && !strings.Contains(state.(file).Mod, "+x") {
-			return &file{
-				Ext: state.(file).Ext,
-				Mod: state.(file).Mod + "+x",
-			}
-		} else if act.Args["mod"] == "+d" && !strings.Contains(state.(file).Mod, "+d") {
-			return &file{
-				Ext: state.(file).Ext,
-				Mod: state.(file).Mod + "+d",
-			}
-		} else if act.Args["mod"] == "-d" {
-			strings.Replace(state.(file).Mod, "+d", "", -1)
-		} else if act.Args["mod"] == "-x" {
-			strings.Replace(state.(file).Mod, "+x", "", -1)
-		} else {
-			return state
-		}
-	case "new ext":
-		if act.Args["Ext"] != nil {
-			return &file{
-				Ext: act.Args["Ext"].(string),
-				Mod: state.(file).Mod,
-			}
-		}
-		return state
-	default:
-		return state
-	}
-	return state
 }
 
 // Make sure Marshal can work with Struct Type
