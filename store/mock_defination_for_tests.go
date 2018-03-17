14efd2dfb3abfb273acd7eb2e4753665a79416ac
routedan <dannypsnl@gmail.com>
2018-03-17 09:17:37 +0800

    refactor: remove reducer fileUpdator(never use)

--- store/mock_defination_for_tests.go
+++ store/mock_defination_for_tests.go
@@ -2,7 +2,6 @@ package store
 
 import (
 	"github.com/dannypsnl/redux/action"
-	"strings"
 )
 
 func counter(state interface{}, action action.Action) interface{} {
@@ -36,51 +35,4 @@ func login(state interface{}, action action.Action) interface{} {
 	default:
 		return state
 	}
-}
-
-type file struct {
-	// Ext mean extension
-	Ext string `json:"ext"`
-	// two mod: +x, +d
-	Mod string `json:"mod"`
-}
-
-func fileUpdator(state interface{}, act action.Action) interface{} {
-	if state == nil {
-		return &file{
-			Ext: "elz",
-			Mod: "+x",
-		}
-	}
-	switch act.Type {
-	case "chmod":
-		if act.Args["mod"] == "+x" && !strings.Contains(state.(file).Mod, "+x") {
-			return &file{
-				Ext: state.(file).Ext,
-				Mod: state.(file).Mod + "+x",
-			}
-		} else if act.Args["mod"] == "+d" && !strings.Contains(state.(file).Mod, "+d") {
-			return &file{
-				Ext: state.(file).Ext,
-				Mod: state.(file).Mod + "+d",
-			}
-		} else if act.Args["mod"] == "-d" {
-			strings.Replace(state.(file).Mod, "+d", "", -1)
-		} else if act.Args["mod"] == "-x" {
-			strings.Replace(state.(file).Mod, "+x", "", -1)
-		} else {
-			return state
-		}
-	case "new ext":
-		if act.Args["Ext"] != nil {
-			return &file{
-				Ext: act.Args["Ext"].(string),
-				Mod: state.(file).Mod,
-			}
-		}
-		return state
-	default:
-		return state
-	}
-	return state
 }
