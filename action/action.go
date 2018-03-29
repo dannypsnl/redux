11c953a771d9a28fb3dc79b08db824944f160776
dannypsnl <dannypsnl@gmail.com>
2018-03-30 02:26:30 +0800

    feat: action.Args impl, help define of action more flexible

--- action/action.go
+++ action/action.go
@@ -1,11 +1,22 @@
 package action
 
+// Args help action define more flexible
+//
+// Example:
+// act := action.Action {
+//     Type: "INC",
+//     Args: action.Args {
+//         "payload": 3,
+//     },
+// }
+type Args map[string]interface{}
+
 // Action is a type for reducer to know what should it do by recognize Action::Type
 type Action struct {
 	// Type help user recognize which signal they receive
 	Type string
 	// Args contain context of action, for example, login need account & password
-	Args map[string]interface{}
+	Args Args
 }
 
 // New create a new action and return a pointer to it.
