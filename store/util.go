536ed9a9d896e9751d88bd76b2413795348cd53b
dannypsnl <dannypsnl@gmail.com>
2018-03-31 11:56:48 +0800

    refactor: improve utils.go's comment

--- store/util.go
+++ store/util.go
@@ -10,8 +10,8 @@ import (
 // getReducerName is a helper func to get function's ref name.
 func getReducerName(r reducer) string {
 	fullName := runtime.FuncForPC(reflect.ValueOf(r).Pointer()).Name()
-	// this code will get package.function_name
-	// so we have to drop package part.
-	// package is full path to it
+	// fullName's format is `package.function_name`
+	// we don't want package part.
+	// package is full path(GOPATH/src/package_part) to it
 	return fullName[strings.LastIndexByte(fullName, '.')+1:]
 }
