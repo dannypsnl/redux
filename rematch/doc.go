151667de58b4b79e30186534ed04a24776d7c72a
dannypsnl <dannypsnl@gmail.com>
2018-04-24 10:27:59 +0800

    docs: init rematch API doc

---
+++ rematch/doc.go
@@ -0,0 +1,19 @@
+// rematch provide rematch API
+//
+// Example:
+//   counter := rematch.Reducer {
+//       State: 0,
+//       Reducers: rematch.Reducers {
+//           "INC": func(state interface{}, act action.Action) interface{} {
+//                return state.(int)+act.Args["payload"].(int)
+//            }),
+//            "DEC": func(state interface{}, act action.Action) interface{} {
+//                return state.(int)-act.Args["payload"].(int)
+//            }),
+//       }
+//   }
+//   store := store.New(counter)
+//   store.Dispatch(counter.
+//       Action("INC").
+//           Arg("payload", 10))
+package rematch
