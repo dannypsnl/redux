46e60cdbbba22ffc62d5d2f79a8bb848267076f2
dannypsnl <dannypsnl@gmail.com>
2018-04-24 10:45:11 +0800

    feat: rematch.Reducer.Action

--- rematch/reducer_test.go
+++ rematch/reducer_test.go
@@ -11,6 +11,11 @@ type Reducer struct {
 	State    interface{}
 	Reducers Reducers
 }
+
+func (r *Reducer) Action(typ string) *action.Action {
+	return action.New(typ)
+}
+
 type Reducers map[string]redux.Reducer
 
 func TestRematchReducer(t *testing.T) {
@@ -31,5 +36,11 @@ func TestRematchReducer(t *testing.T) {
 func testInit(t *testing.T, counter Reducer) {
 	if counter.State != 0 {
 		t.Error("initialize bug")
+	}
+}
+func testAction(t *testing.T, counter Reducer) {
+	act := counter.Action("INC").Arg("payload", 10)
+	if act.Type != "INC" || act.Args["payload"] != 10 {
+		t.Error()
 	}
 }
