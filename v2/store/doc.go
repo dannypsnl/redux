f58b33fc6035a768df7bd7670301cc1e103dc2da
dannypsnl <dannypsnl@gmail.com>
2018-05-13 00:58:34 +0800

    docs: v2 store doc init

---
+++ v2/store/doc.go
@@ -0,0 +1,7 @@
+// store is the dirty part of whole Redux system
+//
+// But also is the main logic place
+//
+// The purpose is providing a module that you can just dispatch to it.
+// Without consider the complex behind it
+package store
