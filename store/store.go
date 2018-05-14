b6825f0830f0b4e46d8eb2f3f871c75153bc3715
dannypsnl <dannypsnl@gmail.com>
2018-05-14 16:49:53 +0800

    refactor: reorder & reduce code

--- store/store.go
+++ store/store.go
@@ -11,42 +11,46 @@ type Store struct {
 	state    map[string]reflect.Value
 }
 
+func checkReducer(r reflect.Value) {
+	if r.Kind() == reflect.Invalid {
+		panic("It's an invalid value")
+	}
+
+	// reducer :: (state, action) -> state
+	if r.Type().NumIn() != 2 {
+		panic("reducer should have state & action two parameter, not thing more")
+	}
+	if r.Type().NumOut() != 1 {
+		panic("reducer should return state only")
+	}
+	if r.Type().In(0) != r.Type().Out(0) {
+		panic("reducer should own state with the same type at anytime, if you want have variant value, please using interface")
+	}
+}
+
 func New(reducers ...interface{}) *Store {
 	rs := make([]reflect.Value, 0)
-	stateInit := make(map[string]reflect.Value)
+	initState := make(map[string]reflect.Value)
 	for _, reducer := range reducers {
 		r := reflect.ValueOf(reducer)
-		if r.Kind() == reflect.Invalid {
-			panic("It's an invalid value")
-		}
-
-		// reducer :: (state, action) -> state
-		if r.Type().NumIn() != 2 {
-			panic("reducer should have state & action two parameter, not thing more")
-		}
-		if r.Type().NumOut() != 1 {
-			panic("reducer should return state only")
-		}
-		if r.Type().In(0) != r.Type().Out(0) {
-			panic("reducer should own state with the same type at anytime, if you want have variant value, please using interface")
-		}
-
-		stateType := r.Type().In(0)
+		// If fail any checking, it will panic, so don't try to recover or handling the error
+		checkReducer(r)
 		rs = append(rs, r)
 
-		reducerName := runtime.FuncForPC(r.Pointer()).Name()
+		rName := runtime.FuncForPC(r.Pointer()).Name()
+		rName = rName[strings.LastIndexByte(rName, '.')+1:]
 		res := r.Call(
 			[]reflect.Value{
-				reflect.Zero(stateType),
+				reflect.Zero(r.Type().In(0)),
 				reflect.Zero(r.Type().In(1))})
-		stateInit[reducerName[strings.LastIndexByte(reducerName, '.')+1:]] = res[0]
+		initState[rName] = res[0]
 	}
-	s := &Store{
+	return &Store{
 		reducers: rs,
-		state:    stateInit,
+		state:    initState,
 	}
-	return s
 }
+
 func (s *Store) Dispatch(action interface{}) {
 	for _, r := range s.reducers {
 		rName := runtime.FuncForPC(r.Pointer()).Name()
