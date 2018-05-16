600b18f7c3c4d1d32e4d3226f7660821099ae7bf
dannypsnl <dannypsnl@gmail.com>
2018-05-16 14:34:55 +0800

    test: compare v1 & v2

--- v2/store/benchmark_test.go
+++ v2/store/benchmark_test.go
@@ -2,7 +2,30 @@ package store
 
 import (
 	"testing"
+
+	"github.com/dannypsnl/redux/action"
+	"github.com/dannypsnl/redux/store"
 )
+
+func BenchmarkV1StoreDispatch(b *testing.B) {
+	counter := func(state interface{}, action action.Action) interface{} {
+		if state == nil {
+			return 0
+		}
+		switch action.Type {
+		case "INC":
+			return state.(int) + 1
+		case "DEC":
+			return state.(int) - 1
+		default:
+			return state
+		}
+	}
+	store := store.New(counter)
+	for i := 0; i < b.N; i++ {
+		store.Dispatch(action.New("INC"))
+	}
+}
 
 func BenchmarkStoreDispatch(b *testing.B) {
 	counter := func(state int, action int) int {
@@ -14,6 +37,26 @@ func BenchmarkStoreDispatch(b *testing.B) {
 	}
 }
 
+func BenchmarkV1StoreGetState(b *testing.B) {
+	counter := func(state interface{}, action action.Action) interface{} {
+		if state == nil {
+			return 0
+		}
+		switch action.Type {
+		case "INC":
+			return state.(int) + 1
+		case "DEC":
+			return state.(int) - 1
+		default:
+			return state
+		}
+	}
+	store := store.New(counter)
+	for i := 0; i < b.N; i++ {
+		store.GetState("func1")
+	}
+}
+
 func BenchmarkStoreStateOf(b *testing.B) {
 	counter := func(state int, action int) int {
 		return state + action
@@ -21,6 +64,26 @@ func BenchmarkStoreStateOf(b *testing.B) {
 	store := New(counter)
 	for i := 0; i < b.N; i++ {
 		store.StateOf(counter)
+	}
+}
+
+func BenchmarkV1StoreSubcribe(b *testing.B) {
+	counter := func(state interface{}, action action.Action) interface{} {
+		if state == nil {
+			return 0
+		}
+		switch action.Type {
+		case "INC":
+			return state.(int) + 1
+		case "DEC":
+			return state.(int) - 1
+		default:
+			return state
+		}
+	}
+	store := store.New(counter)
+	for i := 0; i < b.N; i++ {
+		store.Subscribe(func() {})
 	}
 }
 
